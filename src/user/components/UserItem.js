19688fe1e12e9301d61cb0070ac4ab24147ef8ea
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-11-08 00:31:47 +0530

    updated UserItem.js (bug fix)

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -9,7 +9,7 @@ const UserItem = props => {
   return (
     <li className="user-item">
       <Card className="user-item__content">
-        <Link to={`/${props.id}/places`}>
+        <Link to={`/${props.id}/arts`}>
           <div className="user-item__image">
             <Avatar image={props.image} alt={props.name} />
           </div>
