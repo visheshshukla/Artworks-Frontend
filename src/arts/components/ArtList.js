8b721ffbcd340d669dc4884221870b73220c3a17
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-11-09 01:36:46 +0530

    added ArtList component

---
+++ src/arts/components/ArtList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Card from '../../shared/components/UIElements/Card';
+import ArtItem from './ArtItem';
+import './ArtList.css';
+
+const ArtList = props => {
+  if (props.items.length === 0) {
+    return (
+      <div className="art-list center">
+        <Card>
+          <h2>No arts found. Maybe create one?</h2>
+          <button>Share Art</button>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="art-list">
+      {props.items.map(art => (
+        <ArtItem
+          key={art.id}
+          id={art.id}
+          image={art.imageUrl}
+          title={art.title}
+          description={art.description}
+          address={art.address}
+          creatorId={art.creator}
+          coordinates={art.location}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ArtList;
