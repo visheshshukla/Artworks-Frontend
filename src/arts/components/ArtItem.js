9d877ae501d0917cb8eb8fd38a789703cb21a03f
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-11-11 02:36:51 +0530

    updated ArtItem.js

--- src/arts/components/ArtItem.js
+++ src/arts/components/ArtItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
+import Button from '../../shared/components/FormElements/Button';
 import './ArtItem.css';
 
 const ArtItem = props => {
@@ -16,9 +17,9 @@ const ArtItem = props => {
           <p>{props.description}</p>
         </div>
         <div className="art-item__actions">
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/arts/${props.id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
