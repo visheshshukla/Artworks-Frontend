5f073644a57ac6878eeb92c73280e70ffabd29c4
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-11-12 00:10:07 +0530

    updated Modal.js

--- src/shared/components/UIElements/Modal.js
+++ src/shared/components/UIElements/Modal.js
@@ -28,6 +28,21 @@ const ModalOverlay = props => {
   return ReactDOM.createPortal(content, document.getElementById('modal-hook'));
 };
 
+const Modal = props => {
+  return (
+    <React.Fragment>
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      <CSSTransition
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={200}
+        classNames="modal"
+      >
+        <ModalOverlay {...props} />
+      </CSSTransition>
+    </React.Fragment>
+  );
+};
 
-
-export default ModalOverlay;
+export default Modal;
