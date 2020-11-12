3b050f624c854a7174f54e783b555f545e88728e
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-11-13 01:04:52 +0530

    updated ArtItem.js

--- src/arts/components/ArtItem.js
+++ src/arts/components/ArtItem.js
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useState }from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
+import Modal from '../../shared/components/UIElements/Modal';
 import './ArtItem.css';
 
 const ArtItem = props => {
+  const [showMap, setShowMap] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+
+  const closeMapHandler = () => setShowMap(false);
+
   return (
-    <li className="art-item">
-      <Card className="art-item__content">
-        <div className="art-item__image">
-          <img src={props.image} alt={props.title} />
+    <React.Fragment>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="art-item__modal-content"
+        footerClass="art-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <h2>THE MAP!</h2>
         </div>
-        <div className="art-item__info">
-          <h2>{props.title}</h2>
-          <h3>{props.address}</h3>
-          <p>{props.description}</p>
-        </div>
-        <div className="art-item__actions">
-          <Button inverse>VIEW ON MAP</Button>
-          <Button to={`/arts/${props.id}`}>EDIT</Button>
-          <Button danger>DELETE</Button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className="art-item">
+        <Card className="art-item__content">
+          <div className="art-item__image">
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className="art-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="art-item__actions">
+            <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
+            <Button to={`/arts/${props.id}`}>EDIT</Button>
+            <Button danger>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+      </React.Fragment>
   );
 };
 
