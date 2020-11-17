84f2bedf13fce437c162d5e3c2a2ee40a2ade762
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-11-18 01:40:18 +0530

    updated NewArt.js

--- src/arts/pages/NewArt.js
+++ src/arts/pages/NewArt.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import './NewArt.css';
 
 const NewArt = () => {
@@ -10,7 +11,7 @@ const NewArt = () => {
         element="input"
         type="text"
         label="Title"
-        validators={[]}
+        validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
       />
     </form>
