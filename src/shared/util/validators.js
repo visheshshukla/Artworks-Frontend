21bdf1850ef83b2da576d007ad70a87cda29b4f9
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-11-18 01:38:23 +0530

    added validators.js

---
+++ src/shared/util/validators.js
@@ -0,0 +1,29 @@
+const VALIDATOR_TYPE_REQUIRE = 'REQUIRE';
+const VALIDATOR_TYPE_MINLENGTH = 'MINLENGTH';
+const VALIDATOR_TYPE_MAXLENGTH = 'MAXLENGTH';
+
+export const VALIDATOR_REQUIRE = () => ({ type: VALIDATOR_TYPE_REQUIRE });
+export const VALIDATOR_MINLENGTH = val => ({
+  type: VALIDATOR_TYPE_MINLENGTH,
+  val: val
+});
+export const VALIDATOR_MAXLENGTH = val => ({
+  type: VALIDATOR_TYPE_MAXLENGTH,
+  val: val
+});
+
+export const validate = (value, validators) => {
+  let isValid = true;
+  for (const validator of validators) {
+    if (validator.type === VALIDATOR_TYPE_REQUIRE) {
+      isValid = isValid && value.trim().length > 0;
+    }
+    if (validator.type === VALIDATOR_TYPE_MINLENGTH) {
+      isValid = isValid && value.trim().length >= validator.val;
+    }
+    if (validator.type === VALIDATOR_TYPE_MAXLENGTH) {
+      isValid = isValid && value.trim().length <= validator.val;
+    }
+  }
+  return isValid;
+};
