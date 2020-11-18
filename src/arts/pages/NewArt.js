8a606da9c53783f84a0c3c2e964ce821c618aa06
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-11-19 00:21:41 +0530

    updated NewArt.js

--- src/arts/pages/NewArt.js
+++ src/arts/pages/NewArt.js
@@ -8,7 +8,32 @@ import {
 } from '../../shared/util/validators';
 import './NewArt.css';
 
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid }
+        },
+        isValid: formIsValid
+      };
+    default:
+      return state;
+  }
+};
+
 const NewArt = () => {
+
   return (
     <form className="art-form">
       <Input
