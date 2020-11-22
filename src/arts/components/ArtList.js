33133465a0733a30775316c57b83eedd2936b4e2
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-11-23 00:47:15 +0530

    updated ArtList.js

--- src/arts/components/ArtList.js
+++ src/arts/components/ArtList.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import ArtItem from './ArtItem';
+import Button from '../../shared/components/FormElements/Button';
 import './ArtList.css';
 
 const ArtList = props => {
@@ -10,7 +11,7 @@ const ArtList = props => {
       <div className="art-list center">
         <Card>
           <h2>No arts found. Maybe create one?</h2>
-          <button>Share Art</button>
+          <Button to="/arts/new">Share Art</Button>
         </Card>
       </div>
     );
