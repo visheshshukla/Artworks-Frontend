b550a5bb40edafddb5749301f87e0d981810f714
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-11-24 00:17:37 +0530

    updated ArtItem.js

--- src/arts/components/ArtItem.js
+++ src/arts/components/ArtItem.js
@@ -9,9 +9,24 @@ import './ArtItem.css';
 const ArtItem = props => {
   const [showMap, setShowMap] = useState(false);
 
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+
   const openMapHandler = () => setShowMap(true);
 
   const closeMapHandler = () => setShowMap(false);
+
+  const showDeleteWarningHandler = () => {
+    setShowConfirmModal(true);
+  };
+
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(false);
+  };
+
+  const confirmDeleteHandler = () => {
+    setShowConfirmModal(false);
+    console.log('DELETING...');
+  };
 
   return (
     <React.Fragment>
@@ -27,6 +42,27 @@ const ArtItem = props => {
           <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure?"
+        footerClass="art-item__modal-actions"
+        footer={
+          <React.Fragment>
+            <Button inverse onClick={cancelDeleteHandler}>
+              CANCEL
+            </Button>
+            <Button danger onClick={confirmDeleteHandler}>
+              DELETE
+            </Button>
+          </React.Fragment>
+        }
+      >
+        <p>
+          Do you want to proceed and delete this art? Please note that it
+          can't be undone thereafter.
+        </p>
+      </Modal>
       <li className="art-item">
         <Card className="art-item__content">
           <div className="art-item__image">
@@ -40,7 +76,7 @@ const ArtItem = props => {
           <div className="art-item__actions">
             <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
             <Button to={`/arts/${props.id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
+            <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
           </div>
         </Card>
       </li>
