064e3b675c8c5f12d1c140dc8253b96f085c1cf8
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-11-26 00:53:09 +0530

    updated Navlinks.js

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { AuthContext } from '../../context/auth-context';
 import './NavLinks.css';
 
 const NavLinks = props => {
-  return <ul className="nav-links">
+  const auth = useContext(AuthContext);
+
+  return (
+  <ul className="nav-links">
     <li>
       <NavLink to="/" exact>ALL USERS</NavLink>
     </li>
-    <li>
-      <NavLink to="/u1/arts">MY ARTWORKS</NavLink>
-    </li>
-    <li>
-      <NavLink to="/arts/new">ADD ARTWORK</NavLink>
-    </li>
-    <li>
-      <NavLink to="/auth">AUTHENTICATE</NavLink>
-    </li>
+    {auth.isLoggedIn && (
+      <li>
+        <NavLink to="/u1/arts">MY ARTWORKS</NavLink>
+      </li>
+    )}
+    {auth.isLoggedIn && (
+      <li>
+        <NavLink to="/arts/new">ADD ARTWORK</NavLink>
+      </li>
+    )}
+    {!auth.isLoggedIn && (
+      <li>
+        <NavLink to="/auth">AUTHENTICATE</NavLink>
+      </li>
+    )}
+    {auth.isLoggedIn && (
+      <li>
+        <button onClick={auth.logout}>LOGOUT</button>
+      </li>
+    )}
   </ul>
+  )
 };
 
 export default NavLinks;
