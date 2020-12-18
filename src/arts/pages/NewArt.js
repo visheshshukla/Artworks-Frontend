33f78304df1f42224364b908bdd21cd88b7c274d
visheshshukla <engr.visheshshukla@gmail.com>
2020-12-19 01:55:42 +0530

    updated NewArt.js

--- src/arts/pages/NewArt.js
+++ src/arts/pages/NewArt.js
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react';
+import { useHistory } from  'react-router-dom';
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH
@@ -32,6 +35,8 @@ const NewArt = () => {
     false
   );
 
+  const histroy = useHistory();
+
   const artSubmitHandler = async event => {
     event.preventDefault();
     try{
@@ -40,42 +45,49 @@ const NewArt = () => {
       description: formState.inputs.description.value,
       address: formState.inputs.address.value,
       creator: auth.userId
-      }));
+      }),
+      { 'Content-Type':  'application/json' }
+      );
+      histroy.push('/');
     }
     catch (err) {}
   };
 
   return (
-    <form className="art-form" onSubmit={artSubmitHandler}>
-      <Input
-        id="title"
-        element="input"
-        type="text"
-        label="Title"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title."
-        onInput={inputHandler}
-      />
-      <Input
-        id="description"
-        element="textarea"
-        label="Description"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid description (at least 5 characters)."
-        onInput={inputHandler}
-      />
-      <Input
-        id="address"
-        element="input"
-        label="Address"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid address."
-        onInput={inputHandler}
-      />
-      <Button type="submit" disabled={!formState.isValid}>
-        ADD PLACE
-      </Button>
-    </form>
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      <form className="art-form" onSubmit={artSubmitHandler}>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid title."
+          onInput={inputHandler}
+        />
+        <Input
+          id="description"
+          element="textarea"
+          label="Description"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid description (at least 5 characters)."
+          onInput={inputHandler}
+        />
+        <Input
+          id="address"
+          element="input"
+          label="Address"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid address."
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          ADD PLACE
+        </Button>
+      </form>
+    </React.Fragment>
   );
 };
 
