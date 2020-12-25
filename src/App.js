114d8f7e1354a57b46f97fd2bc37f2d2068ef70a
visheshshukla <engr.visheshshukla@gmail.com>
2020-12-26 02:03:43 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
+  
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userId, setUserId] = useState(false);
 
