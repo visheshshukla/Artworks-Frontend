0f727215da83488d328b4e9c18fa686e4fc62c21
visheshshukla <engr.visheshshukla@gmail.com>
2020-12-27 00:05:03 +0530

    updated NewArt.js

--- src/arts/pages/NewArt.js
+++ src/arts/pages/NewArt.js
@@ -5,6 +5,7 @@ import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH
@@ -30,6 +31,10 @@ const NewArt = () => {
       address: {
         value: '',
         isValid: false
+      },
+      image: {
+        value: null,
+        isValid: false
       }
     },
     false
@@ -40,14 +45,13 @@ const NewArt = () => {
   const artSubmitHandler = async event => {
     event.preventDefault();
     try{
-      await sendRequest('http://localhost:5000/api/arts', 'POST', JSON.stringify({
-      title: formState.inputs.title.value,
-      description: formState.inputs.description.value,
-      address: formState.inputs.address.value,
-      creator: auth.userId
-      }),
-      { 'Content-Type':  'application/json' }
-      );
+      const formData = new FormData();
+      formData.append('title', formState.inputs.title.value);
+      formData.append('description', formState.inputs.description.value);
+      formData.append('address', formState.inputs.address.value);
+      formData.append('creator', auth.userId);
+      formData.append('image', formState.inputs.image.value)
+      await sendRequest('http://localhost:5000/api/arts', 'POST', formData);
       histroy.push('/');
     }
     catch (err) {}
@@ -83,6 +87,7 @@ const NewArt = () => {
           errorText="Please enter a valid address."
           onInput={inputHandler}
         />
+        <ImageUpload id="image" onInput={inputHandler} errorText="Please provide an image" />
         <Button type="submit" disabled={!formState.isValid}>
           ADD PLACE
         </Button>
