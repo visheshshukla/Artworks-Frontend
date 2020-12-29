c15f4dbaa3164043c0769eb158fc532d90840df4
visheshshukla <engr.visheshshukla@gmail.com>
2020-12-30 00:55:54 +0530

    updated Auth.js

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -81,7 +81,7 @@ const Auth = () => {
             'Content-Type': 'application/json'
           } 
         );
-        auth.login(responseData.user.id);
+        auth.login(responseData.userId, responseData.token);
         }
         catch(err) {}
 
@@ -98,7 +98,7 @@ const Auth = () => {
         'POST',
         formData
       );
-      auth.login(responseData.user.id);
+      auth.login(responseData.userId, responseData.token);
       }
       catch(err) {}
       
