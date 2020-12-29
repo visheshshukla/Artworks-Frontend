30889781b5afccfe7cafe70d2eb5acf369efbdda
visheshshukla <engr.visheshshukla@gmail.com>
2020-12-30 02:12:00 +0530

    updated NewArt.js

--- src/arts/pages/NewArt.js
+++ src/arts/pages/NewArt.js
@@ -49,7 +49,6 @@ const NewArt = () => {
       formData.append('title', formState.inputs.title.value);
       formData.append('description', formState.inputs.description.value);
       formData.append('address', formState.inputs.address.value);
-      formData.append('creator', auth.userId);
       formData.append('image', formState.inputs.image.value)
       await sendRequest('http://localhost:5000/api/arts', 'POST', formData, {
         Authorization: 'Bearer ' + auth.token
