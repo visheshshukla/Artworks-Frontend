3d8f4943eb97b05fde63230cf96f8515594572cf
visheshshukla <engr.visheshshukla@gmail.com>
2020-12-31 00:47:57 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewArt from './arts/pages/NewArt';
@@ -7,21 +7,11 @@ import UserArts from './arts/pages/UserArts';
 import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/hooks/auth-hook';
 
 const App = () => {
-  
-  const [token, setToken] = useState(false);
-  const [userId, setUserId] = useState(false);
 
-  const login = useCallback((uid, token) => {
-    setToken(token);
-    setUserId(uid);
-  }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setUserId(null);
-  }, []);
+  const { token, login, logout, userId } = useAuth();
 
   let routes;
 
