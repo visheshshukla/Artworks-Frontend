2838df4004f10cc4aa554784412e1a3dc97e601f
visheshshukla <engr.visheshshukla@gmail.com>
2021-01-01 03:34:36 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import Users from './user/pages/Users';
-import NewArt from './arts/pages/NewArt';
-import UpdateArt from './arts/pages/UpdateArt';
-import UserArts from './arts/pages/UserArts';
-import Auth from './user/pages/Auth';
+// import Users from './user/pages/Users';
+// import NewArt from './arts/pages/NewArt';
+// import UpdateArt from './arts/pages/UpdateArt';
+// import UserArts from './arts/pages/UserArts';
+// import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
 import { useAuth } from './shared/hooks/auth-hook';
+import LoadingSpinner from './shared/components/UIElements/LoadingSpinner';
+
+const Users = React.lazy(() => import('./user/pages/Users'));
+const NewArt = React.lazy(() => import('./arts/pages/NewArt'));
+const UpdateArt = React.lazy(() => import('./arts/pages/UpdateArt'));
+const UserArts = React.lazy(() => import('./arts/pages/UserArts'));
+const Auth = React.lazy(() => import('./user/pages/Auth'));
 
 const App = () => {
 
@@ -58,7 +65,11 @@ const App = () => {
     >
       <Router>
         <MainNavigation />
-        <main>{routes}</main>
+        <main>
+          <Suspense fallback={<div className="center"> <LoadingSpinner/> </div>}>
+            {routes}
+          </Suspense>
+        </main>
       </Router>
     </AuthContext.Provider>
   );
